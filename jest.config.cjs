93305c192c655750d7782ce7fa5a0e2b205ad4d1
Carlos Paredes <carlos@paredesrobles.com>
2024-11-11 00:00:13 -0300

    chore(): add jest

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+// jest.config.cjs
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/app/__mocks__/fileMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+};
