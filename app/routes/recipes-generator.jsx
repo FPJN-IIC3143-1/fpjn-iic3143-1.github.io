8db3756caf070b095fae62d33142b19380ed0eb9
Dafnemami <dafne.arriagada@uc.cl>
2024-12-07 13:36:35 -0300

    cleanse of recipes-generator

--- app/routes/recipes-generator.jsx
+++ app/routes/recipes-generator.jsx
@@ -5,27 +5,50 @@ import SearchBar from "../components/searchBar";
 import PurpleButton from "../components/purpleButton";
 import DataCard from "../components/dataCard"
 import NotificationLogOut from "../components/notificationLogOut";
-
 import ellipseBackground from '/images/ellipse-background.png';
 
+const userName = { Name: "Dafne", LastName: "Arriagada" };
+const headerText = "Con hambre? busca una ... ";
+const headerSpanText = "Receta";
+const firstParagraphText = "Te entregamos un listado de recetas que cumplen ";
+const spanParagraphText = "casi perfectamente ";
+const lastParagraphText = "tus requisitos";
 
 export default function RecipiesGenerator() {
 
+  const getRecipeHandler = () => {
+    console.log("Solicitar receta");
+    // GET /recipes?diet=vegan&intolerances=peanut,soy,egg
+
+    
+
+
+  }
+
+
+
   return (
     <div className="generalContainer flex">
-
-      <SideBar userName={{ Name: "Dafne", LastName: "Arriagada" }} />
-
+      <SideBar userName={userName} />
       <div className="ContainerBody relative h-[1100px] grow bg-[#E5E9F0] p-[60px] z-[0] overflow-y-hidden">
-
-        <h1 className="text-3xl text-[#182F40] font-bold">Con hambre? busca una ... <span className="text-7xl font-extralight">Receta</span></h1>
+        <h1 className="text-3xl text-[#182F40] font-bold">
+          {headerText}<span className="text-7xl font-extralight">{headerSpanText}</span>
+        </h1>
 
         <div className="slider&parragraph flex justify-evenly text-[#182F40] mt-[60px]">
           <div className="flex flex-col justify-between items-center">
             <ProteinSlider/>
-            <PurpleButton text={"Solicita una receta"}/>
+            <PurpleButton 
+              text={"Solicita una receta"}
+              onClick={getRecipeHandler}
+            />
           </div>
-          <p className="w-[300px] text-2xl">Te entregamos un listado de recetas que cumplen <span className="font-bold">casi perfectamente</span> tus requisitos</p>
+
+          <p className="w-[300px] text-2xl">
+            {firstParagraphText}
+            <span className="font-bold">{spanParagraphText}</span>
+            {lastParagraphText}
+          </p>
         </div>
 
         <div className="flex justify-evenly text-[#182F40] mt-[100px]">
@@ -35,7 +58,6 @@ export default function RecipiesGenerator() {
             <div className="h-[20px]"></div>
             <SearchBar/>
           </div>
-
           <div className="flex flex-col items-center">
             <div className="text-3xl font-bold mb-[15px]">Recetas favoritas</div>
             <DataCard useHearts={true} boxWidth={470} leftRowInfo={["Almuerzo", "Desayuno", "Desayuno", "Cena"]} rightRowInfo={["Arroz con pollo al pesto", "Yogurt con semillas y frutas", "Smoothie rojo", "Ensalada de Quinoa"]} />
@@ -45,9 +67,9 @@ export default function RecipiesGenerator() {
         </div>
 
         {/* Background images and Logo */}
-       <img src={ellipseBackground} alt="elipse" className="absolute top-[-35%] left-[-10%] z-[-1]"/>
-       <img src={ellipseBackground} alt="elipse" className="absolute top-[45%] left-[60%] z-[-1]"/>
-       <img src="/images/logo-sin-texto.png" alt="logo" className="fixed bottom-[20px] right-[20px] w-[60px] h-[60px]" />
+        <img src={ellipseBackground} alt="elipse" className="absolute top-[-35%] left-[-10%] z-[-1]"/>
+        <img src={ellipseBackground} alt="elipse" className="absolute top-[45%] left-[60%] z-[-1]"/>
+        <img src="/images/logo-sin-texto.png" alt="logo" className="fixed bottom-[20px] right-[20px] w-[60px] h-[60px]" />
 
       </div>
       <NotificationLogOut />
