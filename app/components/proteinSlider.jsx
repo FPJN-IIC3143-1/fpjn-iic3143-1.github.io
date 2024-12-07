7eb46678c7cb31e999e776343c54f184610b6548
Dafnemami <dafne.arriagada@uc.cl>
2024-12-07 14:46:41 -0300

    protein-slider: actual-value is seen on screen

--- app/components/proteinSlider.jsx
+++ app/components/proteinSlider.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 
 
 export default function ProteinSlider() {
+
+  const [sliderValue, setSliderValue] = useState(0);
+
+  const handleSliderChange = (event) => {
+    setSliderValue(event.target.value);
+  }
 
   return (<>
     
   <div className="flex flex-col">
     <div className="flex justify-between mb-[8px]">
       <p>Proteina a ingerir</p>
-      <p>0 - 200 g</p>
+      {sliderValue > 0 ? <p>{sliderValue} g</p> : <p>0 - 200 g</p>}
     </div>
-    <input className="appearance-none h-[5px] w-[330px] bg-[#ffffff] cursor-pointer rounded-[20px]" type="range" min={1} max={200} defaultValue={0}></input>
+    <input 
+      className="appearance-none h-[5px] w-[330px] bg-[#ffffff] cursor-pointer rounded-[20px]" 
+      type="range" 
+      min={0} 
+      max={200} 
+      defaultValue={0}
+      onChange={handleSliderChange}
+      />
   </div>
 
   </>);
