30f4beda6ffe1e57dd9950b1fd39ad874a0dbd7a
Dafnemami <dafne.arriagada@uc.cl>
2024-12-07 14:18:23 -0300

    recipepopUp style: homogenized

--- app/components/recipePopUp.jsx
+++ app/components/recipePopUp.jsx
@@ -19,30 +19,31 @@ export default function RecipePopUp({ num = 0, title = '', ingredients = [], ste
       </button>
 
       {isVisible && (
-        <div id="RecipePopUpContainer" className="RecipePopUpContainer bg-gray-900 bg-opacity-50 w-full h-full rounded-[20px] m-[2px]
-          flex absolute top-0 right-0 left-0 bottom-0 transition-all duration-100 ease-in">
-          <div className="RecipePopUp flex relative justify-center items-center bg-[#4F378B] w-[600px] h-[90%] rounded-[20px] m-auto p-10">
+        <div className="RecipePopUpContainer bg-gray-900 bg-opacity-50 w-full rounded-[20px] m-[2px]
+          flex absolute top-0 right-0 left-0 bottom-0">
+          <div className="RecipePopUp relative justify-center items-center bg-[#4F378B] w-[700px] rounded-[20px] m-auto p-10">
             <button className="CloseButton absolute top-0 right-0 m-5 
-              bg-[#381E72] w-[30px] h-[30px] rounded-[50%] text-[#EADDFF] hover:bg-[#EADDFF] hover:text-[#4F378B] font-bold"
+              bg-[#381E72] w-[40px] h-[40px] rounded-[50%] text-[#EADDFF] hover:bg-[#EADDFF] hover:text-[#4F378B] font-bold"
               onClick={handleClosePopUp}> X </button>
             <div className="RecipePopUpContent flex flex-col justify-start items-left w-full h-full mt-10 overflow-y-auto">
               <div className="RecipePopUpHeader flex flex-col justify-center items-left w-full">
-                <div className="text-2xl text-gray-100 font-bold font-serif">Receta {num}:</div>
+                <div className="text-2xl text-gray-100 font-bold">Receta {num}:</div>
                 <div className="RecipeTitle text-gray-300 text-4xl text-gray-150 ">{title}</div>
               </div>
-              <div className="text-xl text-gray-100 mt-5 font-serif">Ingredientes:</div>
-              <div className="RecipeIngredients text-gray-300 text-sm text-gray-150">
+              <div className="text-xl text-gray-100 font-bold mb-[10px] mt-[30px]">Ingredientes:</div>
+              <div className="RecipeIngredients text-base text-gray-300">
                 {ingredients.map((ingredient, index) => (
                   <div key={index} className="IngredientItem">• {ingredient}</div>
                 ))}
               </div>
-              <div className="text-xl text-gray-100 mt-5 font-serif">Pasos:</div>
-              <div className="RecipeSteps text-gray-300 text-sm text-gray-150">
+              <div className="text-xl text-gray-100 font-bold mb-[10px] mt-[20px]">Pasos:</div>
+              <div className="RecipeSteps text-gray-100 text-base">
                 {steps.map((step, index) => (
                   <div key={index} className="StepItem">{index + 1}. {step}</div>
                 ))}
               </div>
             </div>
+            <div className="RecipePopUpFooter flex justify-center items-center h-[2px] w-full bg-gray-100 mt-[60px] mb-[10px]"></div>
           </div>
         </div>
       )}
